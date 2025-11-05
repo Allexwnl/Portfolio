15e8464eafc50bc80a80891cd1b8756c2fd38ab6
alexander <alexanderzoet@gmail.com>
2025-11-05 11:24:06 +0100

    fully working database

--- portfolio-project/src/components/NavBar.vue
+++ portfolio-project/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="--navbar">
     <ul>
-      <li><h3 class="font-bold">Alexander Zoet</h3></li>
+      <li><h3 class="font-bold"><a href="/">Alexander Zoet</a></h3></li>
     </ul>
     <ul class="flex flex-row gap-8">
       <li>Projects</li>
